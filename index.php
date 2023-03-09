ac40c0686c8a3b2df9a3d98929f41e124ba7c9dd
Mufaddal Haji <haji@badricomputers.co.in>
2023-03-09 14:35:02 +0530

    icons added

--- index.php
+++ index.php
@@ -6,15 +6,15 @@
 	<title>Service Worker Example</title>
 	<link rel="manifest" href="manifest.json" />
 	<link href="style.css" rel="stylesheet" />
-	<link rel="apple-touch-icon" sizes="180x180" href="image/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="image/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="image/favicon-16x16.png">
-    <link rel="manifest" href="image/site.webmanifest">
+	<link rel="apple-touch-icon" sizes="180x180" href="assets/images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="assets/images/favicon.ico" type="image/x-icon">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon-16x16.png">
     <link rel="mask-icon" href="image/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="apple-mobile-web-app-title" content="Arina Jewellery">
     <meta name="application-name" content="Arina Jewellery">
     <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#ffffff">
+    <meta name="theme-color" content="#000000">
 </head>
 
 <body>
